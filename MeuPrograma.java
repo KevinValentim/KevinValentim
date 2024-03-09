0a8f85cc10713b4d2935bea4041168851121d239
KevinValentim <162632145+KevinValentim@users.noreply.github.com>
2024-03-09 10:51:25 -0300

    Add files via upload

---
+++ MeuPrograma.java
@@ -0,0 +1,7 @@
+class MeuPrograma{
+    public static void main(String[] args){
+        System.out.println("\nMinha primeira aplicação Java!\n");
+        System.out.println("\n...\n");
+        System.out.println("\nAgora foi!\n");
+    }
+}
